cb1ee1af497cd9c8a35034a56ab95d0eedcad7a1
David Medek <cantonae123@gmail.com>
2020-10-07 21:49:07 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const countDownDate = new Date("Dec 24, 2020, 15:00:00").getTime();
+
+const mainFunction = setInterval(() => {
+    const nowTime = new Date().getTime();
+    const distance = countDownDate - nowTime;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const mins = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    const seconds = Math.floor(distance % (1000 * 60) / (1000));
+
+    document.getElementById('countdown-clock').innerHTML = `${days} Days ${hours} Hours ${mins} Minutes ${seconds} Seconds`;
+
+        if(distance < 0 ) {
+            clearInterval(mainFunction);
+            document.getElementById('countdown-clock').innerHTML = "Vánoce jsou tady"
+        }
+},1000)
+
+console.log(countDownDate)
