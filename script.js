7a82148a270d8713fefa38010d838d8a0bfec30c
David Medek <cantonae123@gmail.com>
2020-10-08 16:42:31 +0200

    Snowing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countDownDate = new Date("Dec 24, 2020, 15:00:00").getTime();
-const test = new Date("Dec 24, 2020, 15:00");
+const test = new Date("Oct 10, 2020, 12:05");
 const mainFunction = setInterval(() => {
     const nowTime = new Date().getTime();
     const distance = countDownDate - nowTime;
@@ -9,12 +9,14 @@ const mainFunction = setInterval(() => {
     const mins = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
     const seconds = Math.floor(distance % (1000 * 60) / (1000));
 
-    document.getElementById('countdown-clock').innerHTML = `${days} Days ${hours} Hours ${mins} Minutes ${seconds} Seconds`;
-
+    document.getElementById('countdown-clock').innerHTML = `${days} Dnů ${hours} Hodin ${mins} Minut ${seconds} Sekund`;
+    document.getElementById('finish').innerHTML = `Do Vánoc`;
         if(distance < 0 ) {
             clearInterval(mainFunction);
-            document.getElementById('countdown-clock').innerHTML = "Vánoce jsou tady"
+            document.getElementById('countdown-clock').innerHTML = ""
+            document.getElementById('finish').innerHTML = `Vánoce jsou tady`;
         }
 },1000)
 
 console.log(countDownDate)
+
